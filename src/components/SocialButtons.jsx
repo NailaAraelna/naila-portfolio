4c865db3af7cc74ad1997d08090024c88b20ed59
NailaAraelna <nailaaraelna@gmail.com>
2026-06-02 19:37:31 +0700

    make social button at bottom-right

--- src/components/SocialButtons.jsx
+++ src/components/SocialButtons.jsx
@@ -24,7 +24,7 @@ export default function SocialButtons() {
   ];
 
   return (
-    <div className="fixed bottom-6 left-1/2 -translate-x-1/2 z-50">
+    <div className="fixed bottom-6 right-0 -translate-x-1/3 z-50">
       
       <div className="flex gap-15 px-8 py-3 rounded-full 
                       bg-white/10 backdrop-blur-xl 
